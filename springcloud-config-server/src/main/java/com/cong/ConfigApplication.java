0f0c68d89f4fbed10cd4077107d859d1ef689eec
CoderCong <yicong75932@163.com>
2017-11-23 19:43:23 +0800

    spring-cloud-config-server

---
+++ springcloud-config-server/src/main/java/com/cong/ConfigApplication.java
@@ -0,0 +1,17 @@
+package com.cong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author 759329342@qq.com
+ * @since created  on  2017/11/23.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
